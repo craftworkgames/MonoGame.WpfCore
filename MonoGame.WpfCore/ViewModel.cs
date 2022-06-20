43d9040896fd7ed70f3aa10c859cf27eecad2aa1
FaustVX <jodu035@gmail.com>
2022-06-20 03:22:43 +0200

    Changed to file-scoped namespace

--- MonoGame.WpfCore/ViewModel.cs
+++ MonoGame.WpfCore/ViewModel.cs
@@ -1,15 +1,14 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace MonoGame.WpfCore
-{
-    public class ViewModel : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler? PropertyChanged;
+namespace MonoGame.WpfCore;
 
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null!)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+public class ViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null!)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
